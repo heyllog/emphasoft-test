2017d209e01ae99e0abcbed8f52d02e865f83f5e
Heyllog <heyllog@gmail.com>
2020-07-30 16:48:43 +0300

    Preparing for project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Authorization</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/users">
+          </Route>
+          <Route path="/">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
