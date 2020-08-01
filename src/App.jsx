262abae2e002c8fab3cd41b96bcfbbf38e2b07af
Heyllog <heyllog@gmail.com>
2020-08-01 14:02:21 +0300

    Add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,24 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import AuthPage from './pages/AuthPage';
 import UsersPage from './pages/UsersPage';
+import NotFound from './pages/NotFound';
 
 function App() {
   const [token, setToken] = useState();
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Authorization</Link>
-            </li>
-            <li>
-              <Link to='/users'>Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path='/users'>
-            <UsersPage token={token} />
-          </Route>
-          <Route path='/'>
-            <AuthPage token={token} setToken={setToken} />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/'>
+          <AuthPage token={token} setToken={setToken} />
+        </Route>
+        <Route path='/users'>
+          <UsersPage token={token} />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+  return <p className='not-found'>UH OH! You&apos;re lost.</p>;
+}
+
+export default NotFound;
